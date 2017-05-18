c50c6fc6f8ecbdc5fd001acfe22ca40ca3d0bd8c
Patrícia Pedrosa <Patrcia Pedrosa>
2017-05-18 19:35:15 +0100

    skjhdflç

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -13,4 +13,24 @@ class RequestController extends Controller
     	$requests = Pedido::all()->take(5);
     	return view('showPedidos',compact('requests'));
     }
+
+    public function edit(Request $requests)
+    {
+        $this->authorize('update', $requests);
+        return view('requests.edit', compact('requests'));
+    }
+
+    public function update(Request $requests)
+    {
+        $this->authorize('update', $requests);
+       
+        $requests->save();
+
+        return redirect()
+            ->route('requests.showRequests')
+            ->with('success', 'Request saved successfully');
+    }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -11,37 +11,30 @@
 |
 */
 //---------Request-----------------
-/*Route::get('/requests', 'RequestController@showRequests')->name('requests.showRequests');
+Route::get('/requests', 'RequestController@showRequests')->name('requests.showRequests');
 
-Route::get('requests/{requests}/edit', 'RequestController@edit')->name('requests.edit')
+Route::get('requests/{requests}/edit', 'RequestController@edit')->name('requests.edit');
 
 Route::put('requests/{requests}/edit', 'RequestController@update')->name('requests.update');
 
-Route::delete('$requests/{requests}', 'RequestController@destroy')->name('requests.destroy');
+Route::delete('requests/{requests}', 'RequestController@destroy')->name('requests.destroy');
+
 
 //-----------users----------------
 Route::get('users/{user}/edit', 'UserController@edit')->name('users.edit');
 Route::put('users/{user}/edit', 'UserController@update')->name('users.update');
 
 Route::get('users/create', 'UserController@create')->name('users.create');
-
-
 Route::post('users/create', 'UserController@store')->name('users.store');
-*/
-//Route::get('/listUsers', 'UserController@showUsers')->name('users.showUsers');
 
-
-Route::get('/requests', 'RequestController@showRequests');
-Route::get('/listUsers', 'UserController@showUsers');
+Route::get('/listUsers', 'UserController@showUsers')->name('users.showUsers');
 
 
 //--------------department----------
 
-/*
-
 Route::get('/department', 'DepartmentController@showDepartment')->name('department.showDepartment');
 
-*/
+
 
 
 Auth::routes();
@@ -50,5 +43,5 @@ Auth::routes();
 
 Route::get('/', function () {
     return redirect()->route('users.showUsers');
-});
-*/
+});*/
+
