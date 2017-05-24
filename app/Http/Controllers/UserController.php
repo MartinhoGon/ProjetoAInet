815bff65e110faf2c0dd448ea7cc4b865943d480
Martinho <martinhogon@gmail.com>
2017-05-24 11:06:56 +0100

    e

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
     {
     	$users = User::all();
     	$departments = Department::all();
-    	return view('home',compact('users', 'departments'));
+    	return view('listAll_Users',compact('users', 'departments'));
     }
 
     public function edit(Request $users)
