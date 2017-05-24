2e72bbbd40246fbc5c3026ab46e7c159e2ca5507
Rute Ferreira <leiteferreirarute@gmail.com>
2017-05-24 13:53:18 +0100

    ESTATISTICAS QUASE QUASE

--- app/Http/Controllers/StatisticsRequestsController.php
+++ app/Http/Controllers/StatisticsRequestsController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Department;
-use App\Request as Pedido;
+use App\Requests as Pedido;
 use Khill\Lavacharts\Lavacharts;
+use Lava;
 
 class StatisticsRequestsController extends Controller
 {
     public function showStatistics(){
 
-       
 
         //total impressoes P&B versus Cores
         $this -> showStatisticsColor();
@@ -28,16 +28,23 @@ class StatisticsRequestsController extends Controller
 
     //total impressoes P&B versus Cores
      public function showStatisticsColor(){
+        //sql
+        $countBlack = Pedido::where('colored', 0)->count();
 
-        $lava = new Lavacharts; 
-        $round = $lava->DataTable();
+        $countTotal = Pedido::count();
+
+        $black=($countBlack/$countTotal)*100;
+        $colored = 100-$black;
+
+        //grafico
+        $round = Lava::DataTable();
 
         $round->addStringColumn('Percentagem')
         ->addNumberColumn('Percent')
-        ->addRow(['P&B', 95])
-        ->addRow(['Cores', 5]);
+        ->addRow(['P&B', $black])
+        ->addRow(['Cores', $colored]);
 
-        $lava->DonutChart('Coloracao', $round, [
+        Lava::DonutChart('Coloracao', $round, [
         'title' => 'Percentagem de impressões a Preto e Branco vs a Cores']);
 
         
@@ -45,28 +52,35 @@ class StatisticsRequestsController extends Controller
 
     //total impressoes, de impressoes por departamento, de impressores no corrente dia
      public function showStatisticsTotal(){
+
         $departments = Department::all();
 
-        $lava = new Lavacharts; 
-        $total = $lava->DataTable();
+        //sql
+        $countTotal = Pedido::count();
+        /*
+        $countTotalToday = Pedido::where('DATE(created_at)', CURDATE())->count();
+
+        
+         faltaODep: funcao recebe id do dep e retorna o total de impressoes do dep*/
+
+        //grafico
+        $total = Lava::DataTable();
 
         $total->addStringColumn('Total')
         ->addNumberColumn('Number')
-        ->addRow(['Desde sempre', 200])
+        ->addRow(['Desde sempre', $countTotal])
         ->addRow(['De hoje', 50]);
         foreach ($departments as $dep)
-            $total->addRow(['Departamento de $dep->name', 160]);
+            $total->addRow([$dep->name, 160]);
 
-        $lava->BarChart('Number', $total, [
+        Lava::BarChart('Number', $total, [
         'title' => 'Total de impressões']);
 
 
     }
 
     //média diária de impressões do corrente mês
-      public function showStatisticsAVG(){
-        
-         
+      public function showStatisticsAVG(){         
 
       }
 }
